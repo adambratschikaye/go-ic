198d40ef79ec5f28620c7c191920a5923655116d
Adam Bratschi-Kaye <adam.bratschikaye@dfinity.org>
2024-03-04 11:08:41 +0100

    remove unneeded files

--- lib/rust_lib.h
+++
@@ -1,7 +0,0 @@
-// NOTE: You could use https://michael-f-bryan.github.io/rust-ffi-guide/cbindgen.html to generate
-// this header automatically from your Rust code.  But for now, we'll just write it by hand.
-
-#include <stddef.h>
-
-void init(unsigned char *seed, size_t seed_len);
-int number();
